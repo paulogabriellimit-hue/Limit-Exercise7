61ec3a9f40ffd836f24d60afda27d08d3b17f138
paulogabriellimit-hue <paulogabriel.limit@hcdc.edu.ph>
2026-04-04 15:59:56 +0800

    Initial commit

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,11 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
+  apiKey: "YOUR_REAL_API_KEY",
+  authDomain: "limit-exercise7.firebaseapp.com",
+  projectId: "limit-exercise7",
+  storageBucket: "limit-exercise7.appspot.com",
   messagingSenderId: "YOUR_ID",
   appId: "YOUR_APP_ID",
 };
@@ -13,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const db = getFirestore(app);
